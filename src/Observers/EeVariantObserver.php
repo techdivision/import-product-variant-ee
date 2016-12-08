38101bf2312a34707c465417d193c1dfc14096e6
wagnert <wagner_tim78@hotmail.com>
2016-12-08 10:12:58 +0100

    Initial Release

---
+++ src/Observers/EeVariantObserver.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * TechDivision\Import\Product\Variant\Ee\Observers\EeVariantObserver
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/wagnert/csv-import
+ * @link      http://www.appserver.io
+ */
+
+namespace TechDivision\Import\Product\Variant\Ee\Observers;
+
+use TechDivision\Import\Product\Variant\Observers\VariantObserver;
+
+/**
+ * A SLSB that handles the process to import product bunches.
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/wagnert/csv-import
+ * @link      http://www.appserver.io
+ */
+class EeVariantObserver extends VariantObserver
+{
+
+    /**
+     * {@inheritDoc}
+     * @see \Importer\Csv\Actions\Listeners\Row\ListenerInterface::handle()
+     */
+    public function handle(array $row)
+    {
+
+        // load the header information
+        $headers = $this->getHeaders();
+
+        // extract the parent/child ID as well as option value and variation label from the row
+        $parentSku = $row[$headers[ColumnKeys::VARIANT_PARENT_SKU]];
+        $childSku = $row[$headers[ColumnKeys::VARIANT_CHILD_SKU]];
+        $optionValue = $row[$headers[ColumnKeys::VARIANT_OPTION_VALUE]];
+        $variationLabel = $row[$headers[ColumnKeys::VARIANT_VARIATION_LABEL]];
+
+        // load parent/child IDs
+        $parentId = $this->mapSkuToEntityId($parentSku);
+        $childId = $this->mapSkuToRowId($childSku);
+
+        // create the product relation
+        $this->persistProductRelation(array($parentId, $childId));
+        $this->persistProductSuperLink(array($childId, $parentId));
+
+        // load the store ID
+        $store = $this->getStoreByStoreCode($row[$headers[ColumnKeys::STORE_VIEW_CODE]] ?: StoreViewCodes::ADMIN);
+        $storeId = $store[MemberNames::STORE_ID];
+
+        // load the EAV attribute
+        $eavAttribute = $this->getEavAttributeByOptionValueAndStoreId($optionValue, $storeId);
+
+        // query whether or not, the parent ID have changed
+        if (!$this->isParentId($parentId)) {
+            // preserve the parent ID
+            $this->setParentId($parentId);
+
+            // load the attribute ID
+            $attributeId = $eavAttribute[MemberNames::ATTRIBUTE_ID];
+            // if yes, create the super attribute load the ID of the new super attribute
+            $productSuperAttributeId = $this->persistProductSuperAttribute(array($parentId, $attributeId, 0));
+
+            // query whether or not we've to create super attribute labels
+            if (empty($variationLabel)) {
+                $variationLabel = $eavAttribute[MemberNames::FRONTENT_LABEL];
+            }
+
+            // prepare the super attribute label
+            $params = array($productSuperAttributeId, $storeId, 0, $variationLabel);
+            // save the super attribute label
+            $this->persistProductSuperAttributeLabel($params);
+        }
+
+        // returns the row
+        return $row;
+    }
+
+    /**
+     * Return the row ID for the passed SKU.
+     *
+     * @param string $sku The SKU to return the row ID for
+     *
+     * @return integer The mapped row ID
+     * @throws \Exception Is thrown if the SKU is not mapped yet
+     */
+    public function mapSkuToRowId($sku)
+    {
+        return $this->getSubject()->mapSkuToRowId($sku):
+    }
+}
---
+++ src/Subjects/EeVariantSubject.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * TechDivision\Import\Product\Variant\Ee\Subjects\EeVariantSubject
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/wagnert/csv-import
+ * @link      http://www.appserver.io
+ */
+
+namespace TechDivision\Import\Product\Variant\Ee\Subjects;
+
+use TechDivision\Import\Product\Variant\Subjects\VariantSubject;
+
+/**
+ * A SLSB that handles the process to import product variants.
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/wagnert/csv-import
+ * @link      http://www.appserver.io
+ */
+class EeVariantSubject extends VariantSubject
+{
+
+    /**
+     * The mapping for the SKUs to the created entity IDs.
+     *
+     * @var array
+     */
+    protected $skuRowIdMapping = array();
+
+    /**
+     * Intializes the previously loaded global data for exactly one variants.
+     *
+     * @return void
+     * @see \Importer\Csv\Actions\ProductImportAction::prepare()
+     */
+    public function setUp()
+    {
+
+        // load the entity manager and the registry processor
+        $registryProcessor = $this->getRegistryProcessor();
+
+        // load the status of the actual import process
+        $status = $registryProcessor->getAttribute($this->serial);
+
+        // load the attribute set we've prepared intially
+        $this->skuRowIdMapping = $status['skuRowIdMapping'];
+
+        // prepare the callbacks
+        parent::setUp();
+    }
+
+    /**
+     * Return the row ID for the passed SKU.
+     *
+     * @param string $sku The SKU to return the row ID for
+     *
+     * @return integer The mapped row ID
+     * @throws \Exception Is thrown if the SKU is not mapped yet
+     */
+    public function mapSkuToRowId($sku)
+    {
+
+        // query weather or not the SKU has been mapped
+        if (isset($this->skuRowIdMapping[$sku])) {
+            return $this->skuRowIdMapping[$sku];
+        }
+
+        // throw an exception if the SKU has not been mapped yet
+        throw new \Exception(sprintf('Found not mapped SKU %s', $sku));
+    }
+}
