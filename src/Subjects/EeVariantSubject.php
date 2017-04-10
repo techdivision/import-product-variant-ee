4e689555c1ecd40240bf5d6b6c95a81ddb6d8a92
wagnert <wagner_tim78@hotmail.com>
2017-04-10 11:45:48 +0200

    Refactoring for DI integration

--- src/Subjects/EeVariantSubject.php
+++ src/Subjects/EeVariantSubject.php
@@ -45,14 +45,16 @@ class EeVariantSubject extends VariantSubject
     /**
      * Intializes the previously loaded global data for exactly one variants.
      *
+     * @param string $serial The serial of the actual import
+     *
      * @return void
      * @see \Importer\Csv\Actions\ProductImportAction::prepare()
      */
-    public function setUp()
+    public function setUp($serial)
     {
 
         // invoke the parent method
-        parent::setUp();
+        parent::setUp($serial);
 
         // load the entity manager and the registry processor
         $registryProcessor = $this->getRegistryProcessor();
