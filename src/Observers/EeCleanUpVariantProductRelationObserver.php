fa90c88dbd93d13508dce1ef296b58d8adee0dbf
Tim Wagner <t.wagner@techdivision.com>
2020-11-10 18:19:33 +0100

    * Minor refactoring

--- src/Observers/EeCleanUpVariantProductRelationObserver.php
+++ src/Observers/EeCleanUpVariantProductRelationObserver.php
@@ -14,22 +14,21 @@
  * @author    Martin Eisenführer <m.eisenfuehrer@techdivision.com>
  * @copyright 2020 TechDivision GmbH <info@techdivision.com>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link      https://github.com/techdivision/import-product-media
+ * @link      https://github.com/techdivision/import-product-variant
  * @link      http://www.techdivision.com
  */
 
 namespace TechDivision\Import\Product\Variant\Ee\Observers;
 
-use TechDivision\Import\Product\Ee\Utils\MemberNames;
 use TechDivision\Import\Product\Variant\Observers\CleanUpVariantProductRelationObserver;
 
 /**
- * Observer that cleaned up a product's media gallery information.
+ * Observer that cleaned up a product's variant information.
  *
  * @author    Martin Eisenführer <m.eisenfuehrer@techdivision.com>
  * @copyright 2020 TechDivision GmbH <info@techdivision.com>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link      https://github.com/techdivision/import-product-media
+ * @link      https://github.com/techdivision/import-product-variant
  * @link      http://www.techdivision.com
  */
 class EeCleanUpVariantProductRelationObserver extends CleanUpVariantProductRelationObserver
